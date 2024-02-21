205292239bb410af5a284560e6971e34fdfbb3e7
root <root@duongpq-sandbox.bnh.vn>
2024-02-20 23:29:45 -0500

    add custom index of table

--- main.py
+++ main.py
@@ -5,17 +5,8 @@ from ddl import create_ddl
 from stream import create_stream
 from connector import create_connector
 
-BUILD_FOLDER = "build"
 
-
-def main(table_name):
-    try:
-        raw_index = len(os.listdir(BUILD_FOLDER)) + 1
-    except OSError:
-        raw_index = 1
-
-    table_index = "{:02d}".format(raw_index)
-
+def main(table_index, table_name):
     env_file_content = (
         read_file_content(".env")
         .replace("{INDEX}", table_index)
@@ -42,9 +33,11 @@ def main(table_name):
 
 
 if __name__ == "__main__":
+    BUILD_FOLDER = "build"
     if os.path.exists(BUILD_FOLDER):
         shutil.rmtree(BUILD_FOLDER)
 
-    for table_name in read_file_content("data/table_name.txt").split("\n"):
-        if table_name:
-            main(table_name.strip())
+    for table_info in read_file_content("data/table_name.txt").split("\n"):
+        if table_info:
+            table_index, table_name = table_info.split(".")
+            main("{:02d}".format(int(table_index.strip())), table_name.strip())
